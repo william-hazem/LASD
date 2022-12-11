c9c1a988cd244c6734a4250efa7dc20ff68052e6
William Henrique <william.martins@ee.ufcg.edu.br>
2022-12-11 12:20:18 -0300

    sprint 7

--- Mod_Teste.v
+++ Mod_Teste.v
@@ -38,21 +38,36 @@ LCD_TEST MyLCD (
 	
 	/// Barramento de dados do banco de registro para o Display LCD
 	wire [63:0]LCDBUS = {w_d0x0,w_d0x1,w_d0x2,w_d0x3,w_d1x0,w_d1x1,w_d1x2,w_d1x3};
-	assign w_d0x4 = wPC;
+	assign w_d0x4 = w_PC;
 	
-	wire clk, Z;
+	
+	
+	
+	
+	wire clk, w_Z;
 	/// Gerador de pulso 1 Hz
-	DivFreq #(.nHz(1)) m_Clock1Hz(.Clk_50MHz(CLOCK_50), .clk_out(clk));
+	DivFreq #(.nHz(2)) m_Clock1Hz(.Clk_50MHz(CLOCK_50), .clk_out(clk));
 	
 	
-	assign LEDG[0] = Z;
-	wire [7:0]wPC, wPC_, w_RData;
-	assign wPC_ = wPC + 1;
+	assign LEDG[0] = w_Z;
+	wire [7:0]w_PC, wPC_, w_RData, w_m1, w_nPC;
 	
-	ModPC PC(.clk(clk), .PCi(wPC_), .PCo(wPC));
+	/// somadores de endereço
+	assign wPC_ = w_PC + 1;
+	wire [7:0] AdderBranch = wPC_ + w_Instr[7:0];
+	
+	/// sinais de controle
+	wire w_PCSrc = w_Branch & w_Z;
+	
+	/// Seletores de endereço
+	Mux2 MuxPCSrc(.x0(wPC_), .x1(AdderBranch),.Y(w_m1), .sel(w_PCSrc));
+	Mux2 MuxJump(.x0(w_m1), .x1(w_Instr[7:0]),.Y(w_nPC), .sel(w_Jump));
+	
+	
+	ModPC PC(.clk(clk), .PCi(w_nPC), .PCo(w_PC));
 	
 	wire [31:0] w_Instr;
-	RomInstMem(.address(wPC), .clock(CLOCK_50), .q(w_Instr));
+	RomInstMem(.address(w_PC), .clock(CLOCK_50), .q(w_Instr));
 	RamDataMem DataMem(
 		.address(ULAResult),
 		.clock(CLOCK_50),
@@ -68,13 +83,13 @@ LCD_TEST MyLCD (
 	
 	wire [5:0] OP = w_Instr[31:26];
 	wire [5:0] Funct = w_Instr[5:0];
-	wire RegWrite, RegDst, ULASrc, Branch, MemWrite, MemtoReg, Jump;
+	wire RegWrite, RegDst, ULASrc, w_Branch, MemWrite, MemtoReg, w_Jump;
 	wire [2:0]ULAControl;
 	
 	UnidadeControle ControlUnit(
 		.OP(OP), .Funct(Funct), .RegWrite(RegWrite), 
-		.RegDst(RegDst), .ULASrc(ULASrc), .Branch(Branch), 
-		.MemWrite(MemWrite), .MemtoReg(MemtoReg), .Jump(Jump),
+		.RegDst(RegDst), .ULASrc(ULASrc), .Branch(w_Branch), 
+		.MemWrite(MemWrite), .MemtoReg(MemtoReg), .Jump(w_Jump),
 		.ULAControl(ULAControl)
 	);
 	
@@ -102,9 +117,9 @@ LCD_TEST MyLCD (
 	bcdDecoder(ULAResult[7:4], HEX1);
 	bcdDecoder(data[3:0], HEX4);
 	bcdDecoder(data[7:4], HEX5);
-	ULA ula(.SrcA(srcA), .SrcB(srcB), .ULAControl(ULAControl), .ULAResult(ULAResult), .Z(Z));
+	ULA ula(.SrcA(srcA), .SrcB(srcB), .ULAControl(ULAControl), .ULAResult(ULAResult), .Z(w_Z));
 	
-	assign LEDR = {RegWrite, RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump};
+	assign LEDR = {RegWrite, RegDst,ULASrc,ULAControl,w_Branch,MemWrite,MemtoReg,w_Jump};
 	
 
 endmodule
